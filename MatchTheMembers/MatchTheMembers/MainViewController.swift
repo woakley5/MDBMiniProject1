80890b066b8195e66128eb6de92039fe7b6aa075
Will Oakley <woakley5@icloud.com>
2018-02-07 10:57:18 -0800

    Started working on main play screen

--- MatchTheMembers/MatchTheMembers/MainViewController.swift
+++ MatchTheMembers/MatchTheMembers/MainViewController.swift
@@ -11,10 +11,67 @@ import UIKit
 class MainViewController: UIViewController {
     
     var mainImageView: UIImageView!
+    var optionButtons: [UIButton]!
+    var selectLabel: UILabel!
+    var scoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //// Navigation bar settings
+        self.navigationItem.title = "Match the Members!"
+        
+        /// Button for stats screen
+        let statsButton = UIButton(type: UIButtonType.infoDark)
+        statsButton.addTarget(self, action: #selector(showStatsScreen), for: .touchUpInside)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: statsButton)
+        
+        //Image View Setup
+        let imgX = 30
+        let imgY = 90
+        let imgDimension = Int(self.view.frame.width) - (imgX * 2)
+        mainImageView = UIImageView(frame: CGRect(x: imgX, y: imgY, width: imgDimension, height: imgDimension))
+        mainImageView.contentMode = .scaleAspectFit
+        mainImageView.image = #imageLiteral(resourceName: "willoakley")
+        view.addSubview(mainImageView)
+        
+        //Score Label Setup
+        let scoreX = 50
+        let scoreY = imgY + imgDimension + 20
+        let labelWidth = Int(self.view.frame.width) - (scoreX * 2)
+        let labelHeight = 30
+        scoreLabel = UILabel(frame: CGRect(x: scoreX, y: scoreY, width: labelWidth, height: labelHeight))
+        scoreLabel.textAlignment = .center
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+        
+        //Button Setup
+        //  1    2
+        //  3    4
+        for i in 0..<4 {
+            var buttonX: Int!
+            var buttonY: Int!
+            
+            if i <= 1 { //first row of buttons
+                buttonX = 50 + 200*i
+                buttonY = scoreY + labelHeight + 50
+            }
+            else {
+                buttonX = 50 + 200*(i-2)
+                buttonY = scoreY + labelHeight + 150
+            }
+            
+            let buttonWidth = 100
+            
+            let b = UIButton(frame: CGRect(x: buttonX, y: buttonY, width: buttonWidth, height: 50))
+            b.addTarget(self, action: #selector(tappedButton), for: .touchUpInside)
+            b.setTitle("Name " + String(i), for: .normal)
+            b.setTitleColor(.blue, for: .normal)
+            b.backgroundColor = .yellow
+            b.tag = i
+            view.addSubview(b)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,15 +79,17 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func tappedButton(sender: UIButton){
+        print("Tapped Button " + String(sender.tag))
     }
-    */
+    
+    func newImage(){
+        
+    }
+    
+
+    @objc func showStatsScreen(){
+        self.performSegue(withIdentifier: "showStatsScreen", sender: self)
+    }
 
 }
